a3ceca2ca8bbc8aa70b903bb52e6e66bb6fa0d98
hemade <hemade@gmail.com>
2025-03-24 21:12:51 +0200

    #

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,9 +2,20 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/product");
 
+// جلب جميع المنتجات أو منتجات محددة بناءً على cat_prefix
 router.get("/", async (req, res) => {
   try {
-    const products = await Product.find();
+    const { cat_prefix } = req.query; // استخراج cat_prefix من الاستعلام
+    let products;
+
+    if (cat_prefix) {
+      // جلب المنتجات التي تطابق cat_prefix
+      products = await Product.find({ cat_prefix });
+    } else {
+      // جلب جميع المنتجات إذا لم يتم تحديد cat_prefix
+      products = await Product.find();
+    }
+
     res.json(products);
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
