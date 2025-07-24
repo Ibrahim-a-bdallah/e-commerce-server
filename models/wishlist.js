1ad3c7ae13a2022453be1889743f39724ee9c31d
hemade <hemade@gmail.com>
2025-07-24 13:45:40 +0300

    first commit

--- models/wishlist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const wishlistSchema = new mongoose.Schema({
-  userId: Number,
-  productId: Number,
-  id: Number,
-});
-
-module.exports = mongoose.model("Wishlist", wishlistSchema);
